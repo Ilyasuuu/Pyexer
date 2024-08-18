f336e9a1e806a3b8acf403f1ed636b6ba99d585b
Ilyas El <elbourari4@gmail.com>
2024-08-18 21:06:44 +0300

    ex24

--- Python-exer.py
+++ Python-exer.py
@@ -417,7 +417,7 @@ Exercise 2: Group Anagrams
 Objective:
 Given a list of strings, write a function that groups anagrams together
 '''  
-from collections import defaultdict 
+'''from collections import defaultdict 
 def group_anagrams(words):
     anagrams = defaultdict(list)
     for word in words:
@@ -429,4 +429,80 @@ def group_anagrams(words):
     return list(anagrams.values())
 
 S = ["eat", "tea", "tan", "ate", "nat", "bat"]
-print(group_anagrams(S))     #ouput : [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+print(group_anagrams(S))     #ouput : [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]'''
+
+"""
+Exercise: Implement a LRU (Least Recently Used) Cache
+Objective:
+Implement a Least Recently Used (LRU) cache mechanism using Python.
+
+Description:
+The LRU cache should support the following operations:
+
+get(key): Retrieve the value associated with the key if the key exists in the cache. Otherwise, return -1.
+put(key, value): Insert or update the value associated with the key.
+If the cache reaches its capacity, it should remove the least recently used item before inserting a new item.
+"""
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # maps key to node
+        self.head = Node(0, 0)  # dummy head
+        self.tail = Node(0, 0)  # dummy tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        """Remove a node from the linked list."""
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _add(self, node):
+        """Add a node right after the head."""
+        next_node = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = next_node
+        next_node.prev = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._add(node)
+        self.cache[key] = node
+
+        if len(self.cache) > self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
+
+# Example usage:
+cache = LRUCache(2)
+
+cache.put(1, 1)  # cache is {1=1}
+cache.put(2, 2)  # cache is {1=1, 2=2}
+print(cache.get(1))  # returns 1, cache is {2=2, 1=1}
+cache.put(3, 3)  # evicts key 2, cache is {1=1, 3=3}
+print(cache.get(2))  # returns -1 (not found)
+cache.put(4, 4)  # evicts key 1, cache is {3=3, 4=4}
+print(cache.get(1))  # returns -1 (not found)
+print(cache.get(3))  # returns 3, cache is {4=4, 3=3}
+print(cache.get(4))  # returns 4, cache is {3=3, 4=4}
