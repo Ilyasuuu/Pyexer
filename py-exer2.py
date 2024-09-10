f9074ae5094e122dc4b19b43794b737dfade3ce1
Ilyas El <elbourari4@gmail.com>
2024-09-10 16:07:56 +0300

    2ex

--- py-exer2.py
+++ py-exer2.py
@@ -1,7 +1,25 @@
 # A pronic number is a number which is the product of two consecutive integers.
 import math
-def is_pronic(n):
+"""def is_pronic(n):
     number = int(math.sqrt(n))
     return n == number * (number + 1)
 
-print(is_pronic(11))    
+print(is_pronic(11))"""
+
+# 2ex: Rotate a Matrix by 90 Degrees Clockwise
+
+def rotate_matrix(matrix):
+    matrix_len = len(matrix)
+    for row in range(matrix_len):
+        for col in range(row, matrix_len):
+            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+    for row in range(matrix_len):
+        matrix[row].reverse()
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]            
+rotate_matrix(matrix)
+print(matrix)
